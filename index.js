01b822ee4a4ec927932fcdb33d0a05a94838efd8
FranciscoMorales015 <francisco.morales813@gmail.com>
2018-03-27 13:33:50 -0300

    1) Creacion de rutas 2) Cracion de login 3) Creacion de middleware

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const http = require('http');
+// const http = require('http');
+
+// http.createServer((req, res) =>{// simbolo de flecha abreviacion de  function
+//     res.end('Hello world');
+// }).listen(port);
+
+const express = require('express');
+const app = express();
 const port = 3000;
-http.createServer((req, res) =>{// simbolo de flecha abreviacion de  function
-    res.end('Hello world');
-}).listen(port);
+const morgan = require('morgan');
+
+//Settings
+app.set('appName', 'Mi primer server');
+app.set('views', __dirname + '/views');
+// console.log(__dirname + '/views');
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(morgan('dev'));
+
+// MIDDLEWARE
+// app.use( (req, res ,next)=>{  //middle wares funciones q s ejecutan lineal si no tiene el next se ejecuta el archivo hasta entre en una middle ware
+//  console.log('request url :' + req.url);
+//  next(); 
+// });
+// app.use( (req, res ,next)=>{  //middle wares funciones q s ejecutan lineal si no tiene el next se ejecuta el archivo hasta entre en una middle ware
+//     console.log('Ha pasado por esta funcion');
+//     next(); 
+//    });
+
+//RUTAS routes
+app.get('/', (req, res)=> {    //req=peticion res= responder
+//     res.end('Hello world');
+res.render('index.ejs');
+});
+app.get('/login', (req, res)=> {  
+    res.render('login.ejs'); 
+    // res.end('Aqui va el login');
+});
+app.get('*', (req, res)=> {    
+    res.end('Ruta no encontrada');
+});
+app.listen(port, ()=>{
+    console.log ('Server ok')
+    console.log ('Nombre de la ap: ', app.get('appName'));
+});
